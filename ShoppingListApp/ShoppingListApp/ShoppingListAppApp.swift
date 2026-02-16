219ba54fc7d3aa489be9823e2573ed90a75832e4
moz134 <md.mozammil134@gmail.com>
2026-02-16 19:45:31 +0530

    Init commit

---
+++ ShoppingListApp/ShoppingListApp/ShoppingListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListAppApp.swift
+//  ShoppingListApp
+//
+//  Created by Md Mozammil on 16/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
